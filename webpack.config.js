d95b4e9ab686b8033b4d1abc49dc1893e1c78111
Andres Cabrera <andres.cabrera@bibamgroup.com>
2018-03-12 12:53:27 -0300

    WEBPACK-11 | Demo from video : Plugins: How to use

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require("path");
+const ExamplePlugin = require("./ExamplePlugin.js");
+const webpack = require("webpack");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -14,5 +17,24 @@ module.exports = {
 	     }
     ]
   },
-  mode: "development" 
+  plugins: [
+    new ExamplePlugin(),
+    new webpack.ContextReplacementPlugin()
+  ], 
+  mode: "production",
+  optimization: {
+    minimizer: [
+      // we specify a custom UglifyJsPlugin here to get source maps in production
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        uglifyOptions: {
+          compress: false,
+          ecma: 6,
+          mangle: true
+        },
+        sourceMap: true
+      })
+    ]
+  }
 }
