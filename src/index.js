32206ca63c5652bd31fa01b4d5723215133f6e7b
Andres Cabrera <andres.cabrera@bibamgroup.com>
2018-03-12 12:22:24 -0300

    WEBPACK-9 | Demo from video : Loaders and Rules: Using your first loader!

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import fooString from "./foo";
 import bar from "./bar-cjs";
+import "./google_todays_doodle.jpeg"
 
 console.log(fooString, bar);
--- webpack.config.js
+++ webpack.config.js
@@ -9,26 +9,8 @@ module.exports = {
   module: {
     rules: [
 	     {
-               test: /\.js$/,
-               use: "babel-loader"
-             },
-             {
-               test: /\.css$/,
-               use: [
-                 "style-loader",
-                 "css-loader"
-               ]
-             },
-	     {
-	       test: /\.jpeg$/,
-               use: [
-                 { 
-                   loader: "url-loader", 
-                   options: {
-                     limit: 10000
-                   } 
-		 } 
-	       ]
+	       test: /\.jpe?g$/,
+               use: ["file-loader"]
 	     }
     ]
   },
